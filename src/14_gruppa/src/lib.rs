ae450e3c5e8e59c6457cad1f159a59329985b7f1
sebastos1 <sebastiandalknu@gmail.com>
2024-01-30 19:01:52 +0000

    typing

--- src/14_gruppa/src/lib.rs
+++ src/14_gruppa/src/lib.rs
@@ -3,7 +3,11 @@
 
 pub mod vga;
 pub mod interrupts;
+pub mod gdt;
 
 pub fn init() {
+    gdt::init();
     interrupts::init_idt();
+    unsafe { interrupts::PICS.lock().initialize() };
+    x86_64::instructions::interrupts::enable();
 }
--- src/14_gruppa/src/main.rs
+++ src/14_gruppa/src/main.rs
@@ -9,16 +9,17 @@ use core::panic::PanicInfo;
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
     
-    os::init(); // new
+    os::init();
 
-    // invoke a breakpoint exception
-    x86_64::instructions::interrupts::int3(); // new
-
-    loop {}
+    loop {
+        x86_64::instructions::hlt(); // just does ("hlt" :::: "volatile")
+    }
 }
 
 /// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+    loop {
+        x86_64::instructions::hlt(); 
+    }
 }
